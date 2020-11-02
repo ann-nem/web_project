1f39ef8d2b1ff4666a424acf9b8924422906a9d8
ann-nem <a.nemshilova@yandex.ru>
2020-11-02 18:16:01 +0300

    Добавлены компоненты для главной страницы

---
+++ lab2/lab2/src/components/HitProduct.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card card-hit"><img class="card-img-top" v-bind:src="hitProduct.image">
+    <div class="card-body">
+      <h5>
+        <router-link v-bind:to="{name: 'Product', params: {url: hitProduct.url}}" class="catalog-title">
+          {{ hitProduct.name }}
+        </router-link>
+      </h5>
+      <p class="card-text">{{ hitProduct.text }}</p>
+    </div>
+    <div class="card-footer"><small class="text-muted"><a href="" class='product-link'>
+      <router-link v-bind:to="{name: 'Product', params: {url: hitProduct.url}}" class="hit-footer">
+        Перейти к товару
+      </router-link>
+    </a></small></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HitProduct",
+  props: ['hitProduct'],
+}
+</script>
--- lab2/lab2/src/views/Home.vue
+++ lab2/lab2/src/views/Home.vue
@@ -3,46 +3,55 @@
     <Carousel></Carousel>
     <div class="jumbotron">
       <h1 class="display-4">Вкусно, стильно, неповторимо</h1>
-      <p class="lead">Каждый десерт для нашего онлайн-магазина производится по уникальным рецептам, а их украшение продумано до мельчайших деталей, чтобы радовать наших клиентов каждый день.</p>
+      <p class="lead">Каждый десерт для нашего онлайн-магазина производится по уникальным рецептам, а их украшение
+        продумано до мельчайших деталей, чтобы радовать наших клиентов каждый день.</p>
       <hr class="my-4">
       <p>Сделайте свой первый заказ наших фирменных десертов уже сейчас.</p>
-      <p class="lead"> <a class="btn btn-primary btn-lg" href="#" role="button">Посмотреть</a> </p>
+      <p class="lead"><a class="btn btn-primary btn-lg" href="#" role="button">Посмотреть</a></p>
     </div>
     <h1 class="display-4 text-center">Хиты недели</h1>
     <div class="card-deck">
-      <div class="card"> <img src="https://madbutterbd.com/images/mad_butter/products/1/image.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Торт "Секрет небес"</h5>
-          <p class="card-text">Десерт, выполненный в нежных голубо-розовых тонах со вкусом клубники со сливками, черники и воздушным суфле внутри.</p>
+      <div class="row">
+        <div class="col-lg-4" v-for="item in items">
+          <HitProduct v-bind:hitProduct="item"></HitProduct>
         </div>
-        <div class="card-footer"> <small class="text-muted"><a href="product.html" class='product-link'>Перейти к товару</a></small> </div>
-      </div>
-      <div class="card"> <img src="https://i.pinimg.com/originals/69/42/b8/6942b8c6c23c9705e82055a5f27e57ce.jpg" class="card-img-top" alt="">
-        <div class="card-body">
-          <h5 class="card-title">Капкейк "Единорог"</h5>
-          <p class="card-text">Порция ярких воздушных капкейков со вкусом фруктов и нежной сливочной шапочкой.</p>
-        </div>
-        <div class="card-footer"> <small class="text-muted"><a href="" class='product-link'>Перейти к товару</a></small> </div>
-      </div>
-      <div class="card"> <img src="https://i.pinimg.com/736x/92/79/f7/9279f78aa0e02295cb9d31d1464dbb38.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Торт "Космос"</h5>
-          <p class="card-text">Выполненный в стильной цветовой гамме торт, основные вкусовые составляющие которого - черничный вкус и шоколадные коржи внутри, а также фирменный крем.</p>
-        </div>
-        <div class="card-footer"> <small class="text-muted"><a href="" class='product-link'>Перейти к товару</a></small> </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Carousel from "@/components/Carousel";
+import HitProduct from "@/components/HitProduct";
 
 export default {
   name: 'Home',
   components: {
-    Carousel
+    Carousel, HitProduct
+  },
+  data() {
+    return {
+      items: [
+        {
+          name: 'Торт "Секрес небес"',
+          text: "Десерт, выполненный в нежных голубо-розовых тонах со вкусом клубники со сливками, черники и воздушным суфле внутри.",
+          url: 'СекретНебес',
+          image: 'https://madbutterbd.com/images/mad_butter/products/1/image.jpg',
+        },
+        {
+          name: 'Капкейки "Единорог"',
+          text: "Порция ярких воздушных капкейков со вкусом фруктов и нежной сливочной шапочкой.",
+          url: 'Единорог',
+          image: 'https://i.pinimg.com/originals/69/42/b8/6942b8c6c23c9705e82055a5f27e57ce.jpg',
+        },
+        {
+          name: 'Торт "Космос"',
+          text: "Выполненный в стильной цветовой гамме торт, основные вкусовые составляющие которого - черничный вкус и шоколадные коржи внутри, а также фирменный крем.",
+          url: 'Космос',
+          image: 'https://i.pinimg.com/736x/92/79/f7/9279f78aa0e02295cb9d31d1464dbb38.jpg',
+        },
+      ]
+    }
   }
 }
 </script>
