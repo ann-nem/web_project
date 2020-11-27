74b6934ad16b29460d2ee6258683feb810aad159
ann-nem <a.nemshilova@yandex.ru>
2020-11-28 00:37:43 +0300

    Развернули базовый проект Yii2

---
+++ lab3/basic/frontend/src/components/Carousel.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-carousel
+      :interval="4000"
+      controls
+      indicators>
+    <b-carousel-slide
+        img-src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+        img-src="https://cdn.pixabay.com/photo/2019/04/19/17/48/cake-balls-4139982_1280.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+        img-src="https://cdn.pixabay.com/photo/2017/11/11/20/32/food-2940374_1280.jpg"
+    ></b-carousel-slide>
+  </b-carousel>
+</template>
+
+<script>
+
+export default {
+name: "Carousel"
+}
+</script>
---
+++ lab3/basic/frontend/src/views/SignIn.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container sign-in">
+    <div class="card card-auto">
+      <div class="card-header text-center"> Авторизация </div>
+      <div class="card-body">
+        <h5 class="card-title text-center">Добро пожаловать</h5>
+        <p class="card-text text-center text-muted">Введите ваш логин и пароль.</p>
+        <form @submit="signIn">
+          <div class="form-group">
+            <label for="email">Адрес электронной почты</label>
+            <input type="email" class="form-control" id="email" v-model="email" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">Мы не расскажем ваш email-адрес кому-либо еще.</small>
+            <div class="error">{{errors.email}}</div>
+          </div>
+          <div class="form-group">
+            <label for="password">Пароль</label>
+            <input type="password" class="form-control" id="password" v-model="password"> </div>
+          <div class="error">{{errors.password}}</div>
+          <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Запомнить</label>
+          </div>
+          <button type="submit" class="btn btn-primary btn-in">Войти</button> </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import User from '@/components/user/user'
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      email: '',
+      password: '',
+      errors: {}
+    }
+  },
+  methods: {
+    signIn(e) {
+      if (this.validate()) {
+        const result = {
+          name: 'Иван Иванов',
+          email: 'ivan@ya.ru',
+          accessToken: 'jsdlf732649823709!ew98r#$432'
+        }
+        User.login(result)
+      }
+      e.preventDefault()
+    },
+    validate() {
+      this.errors = {}
+      if (this.email.trim().length === 0) {
+        this.errors.email = 'Заполните Email.'
+      }
+      if (this.password.trim().length === 0) {
+        this.errors.password = 'Заполните Пароль.'
+      }
+      return Object.keys(this.errors).length === 0
+    }
+  }
+}
+</script>
