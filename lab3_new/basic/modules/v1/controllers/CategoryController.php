ce1ca1d354540094315746b0970c9b9be319c50c
ann-nem <a.nemshilova@yandex.ru>
2020-12-11 16:51:14 +0300

    Созданы контроллеры для продукта и категорий товаров

---
+++ lab3_new/basic/modules/v1/controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use app\modules\v1\models\Category;
+use Yii;
+
+class CategoryController extends ApiController {
+
+    public function actionAll() {
+        return Category::find()->all();
+    }
+
+    public function actionConcrete($id) {
+        return Category::findOne(['id' => $id]);
+    }
+
+    public function actionCreate() {
+        $category = new Category();
+        $category->load(Yii::$app->request->getBodyParams(), '');
+        $category->save();
+
+        return $category;
+    }
+}
---
+++ lab3_new/basic/modules/v1/controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use app\modules\v1\models\Product;
+use Yii;
+
+class ProductController extends ApiController {
+
+    public function actionAll() {
+        return Product::find()->all();
+    }
+
+    public function actionConcrete($id) {
+        return Product::findOne(['id' => $id]);
+    }
+
+    public function actionCreate() {
+        $product = new Product();
+        $product->load(Yii::$app->request->getBodyParams(), '');
+        $product->save();
+
+        return $product;
+    }
+}
