4142089d9ea95e0106f30cf2c963db21ae7a9bcc
ann-nem <a.nemshilova@yandex.ru>
2020-12-19 01:16:31 +0300

    Изменена страница профиля

--- backend/basic/frontend/src/views/Profile.vue
+++ backend/basic/frontend/src/views/Profile.vue
@@ -1,15 +1,15 @@
 <template>
 
-  <div class="jumbotron">
+  <div class="jumbotron jumbotron-in">
     <h2 v-if="user">Вы зашли как {{ user.name }}</h2>
     <h2 v-if="!user">К сожалению, вы еще не зашли.</h2>
     <hr class="my-4">
     <p class="lead" v-if="user">Вы можете сделать заказ на нашем сайте уже сейчас!</p>
     <p class="lead" v-if="!user">Вы можете зайти в свой профиль или зарегестрироваться прямо сейчас.</p>
-    <p class="lead">
-            <button v-if="user" class="btn btn-secondary btn-buy"  v-on:click="signOut">Выйти</button>
-            <button v-if="!user" class="btn btn-secondary btn-buy" v-on:click="routeToSignIn">Войти</button>
-            <button v-if="!user" class="btn btn-secondary btn-buy" v-on:click="routeToSignUp">Зарегестрироваться</button>
+    <p class="lead btn-lead">
+            <button v-if="user" class="btn btn-secondary btn-buy btn-out"  v-on:click="signOut">Выйти</button>
+            <button v-if="!user" class="btn btn-secondary btn-buy btn-in btn-pr" v-on:click="routeToSignIn">Войти</button>
+            <button v-if="!user" class="btn btn-secondary btn-buy btn-pr" v-on:click="routeToSignUp">Зарегестрироваться</button>
     </p>
   </div>
 
